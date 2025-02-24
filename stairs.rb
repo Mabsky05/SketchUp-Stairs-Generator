91ab21f56c078063861b813b5f3ce180552fb89a
Mabsky <grimdango@gmail.com>
2025-02-24 11:56:42 +1100

    Completed functions.Problem: How to integrate?

--- stairs.rb
+++ stairs.rb
@@ -4,9 +4,7 @@
 model = Sketchup.active_model
 ent = model.entities
 
-#always add mm to specify lengths
-
-# code for getting step heights
+#Get step heights
 def list (stair_ht, min, max)
   riser_list = []
   avg_riser_ht = (min + max) / 2
@@ -24,15 +22,16 @@ def list (stair_ht, min, max)
     (riser_list.append(riser_rest))
     risers += 1
   end
+
+  return riser_init
+  return riser_rest
 end
 
+return riser_rest
 
-#code to get points of, and draw stair outline
-#tread is new input
-#TODO subtract from step number (how much?)
-
+#Draw stair outline
 def riser_tread(riser_init, riser_rest, tread, step_number)
-  steps_list = [ [0, 0, 0], [0, 0, riser_init], [0, tread, riser_init], [0, tread, riser_init + riser]]
+  steps_list = [ [0, 0, 0], [0, 0, riser_init], [0, tread, riser_init], [0, tread, riser_init + riser_rest]]
   points_list = []
   riser_start = riser_init + riser_rest
   tread_start = tread
@@ -52,27 +51,11 @@ def riser_tread(riser_init, riser_rest, tread, step_number)
           end          
       end
 
-  # for each point, create a variable p# and append to points_list
+      p steps_list
+      Sketchup.active_model.entities.add_line (steps_list)
+end
+  
+  riser_tread(160.mm, 180.mm, 250.mm, 20)
 
-  p steps_list
-  
-  
-  end
-  
-  riser_tread(160, 170, 250, 5)
-  
-# #test stair
-# #draw stair outline from pts 
-# pta = [0 ,0 ,0]
-# ptb = [0, 250.mm, 0] #bot1]
-# ptc = [0, 0, 150.mm]
-# pt1 = [0, 250.mm, 150.mm]
-# pt2 = [0, 250.mm, 300.mm] 
-# pt3 = [0, 500.mm, 300.mm]
-# pt4 = [0, 500.mm, 450.mm]
-# pt5 = [0, 750.mm, 450.mm]
-# ptd = [0, 750.mm, 300.mm] #bot2
 
-# #extrude to x/y axis
-# stairface = ent.add_face pt1, pt3, pt4, pt5, pt6, pt7, pt8, pt9, pt2, pt1
-# stairface.pushpull 900.mm
+
