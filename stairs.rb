bb6f50adab17ffae1d8f9ffc61506bf606944e0b
Mabsky <grimdango@gmail.com>
2025-02-24 13:04:17 +1100

    Code working. Need to add input prompt and proper step_number

--- stairs.rb
+++ stairs.rb
@@ -5,7 +5,7 @@ model = Sketchup.active_model
 ent = model.entities
 
 #Get step heights
-def list (stair_ht, min, max)
+def list(stair_ht, min, max, step_length)
   riser_list = []
   avg_riser_ht = (min + max) / 2
   step_number = stair_ht / avg_riser_ht
@@ -14,48 +14,42 @@ def list (stair_ht, min, max)
   rem2 = rem1 % (step_number - 1)
   riser_init = avg_riser_ht + rem2
   riser_rest = avg_riser_ht + step_add
+  tread = step_length
   
-  riser_list.append(riser_bottom)
+  def riser_tread(riser_init, riser_rest, step_number, tread)
+    steps_list = [ [0, 0, 0], [0, 0, riser_init], [0, tread, riser_init], [0, tread, riser_init + riser_rest]]
+    points_list = []
+    riser_start = riser_init + riser_rest
+    tread_start = tread
+    
+        steps = 0
+        while steps < step_number
+          steps += 1
   
-  risers = 1
-  while risers < step_number
-    (riser_list.append(riser_rest))
-    risers += 1
+            if steps % 2 == 0
+              riser_start = riser_start + riser_rest
+              steps_list.append([0, tread_start, riser_start])
+    
+            else
+              tread_start = tread_start + tread
+              steps_list.append([0, tread_start, riser_start])
+  
+            end          
+        end
+  
+
+        Sketchup.active_model.entities.add_line (steps_list)
   end
+  riser_tread(riser_init, riser_rest, step_number, tread)
 
-  return riser_init
-  return riser_rest
 end
 
-return riser_rest
 
-#Draw stair outline
-def riser_tread(riser_init, riser_rest, tread, step_number)
-  steps_list = [ [0, 0, 0], [0, 0, riser_init], [0, tread, riser_init], [0, tread, riser_init + riser_rest]]
-  points_list = []
-  riser_start = riser_init + riser_rest
-  tread_start = tread
+list(3000, 160, 180, 250)
+
+
   
-      steps = 0
-      while steps < step_number
-        steps += 1
 
-          if steps % 2 == 0
-            riser_start = riser_start + riser_rest
-            steps_list.append([0, tread_start, riser_start])
-  
-          else
-            tread_start = tread_start + tread
-            steps_list.append([0, tread_start, riser_start])
-
-          end          
-      end
-
-      p steps_list
-      Sketchup.active_model.entities.add_line (steps_list)
-end
-  
-  riser_tread(160.mm, 180.mm, 250.mm, 20)
 
 
 
