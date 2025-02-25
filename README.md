73d29f7b3acee9a7040f63839e8a5cac94ff56b0
Mabsky <grimdango@gmail.com>
2025-02-25 21:10:11 +1100

    log updates and change to Repo name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SketchUp Snippets
+# SketchUp: Stairs given Floor to Ceiling heights
 
 ## Usage
 Extensions > Developer > Ruby Console </br> 
@@ -7,8 +7,35 @@ Ex. "load C:/[username]/[script name.rb]" </br>
 
 Or copy-paste script contents to console.
 
+## Summary 
+Stairs are generated on inputting the following: </br>
+Height (floor to floor/ceiling) </br>
+minimum riser height </br>
+maximum riser height </br>
+tread depth </br>
+
+Steps:</br> 
+1) Divide height by the average of the min and max riser heights to get number of steps</br>
+2) Distribute remainder of quotient to risers, such that heights are evenly distributed
+in reasonable dimensions. In this process all riser heights will be equal, except the first </br>
+3) Draw a (poly)line surface tracing the stair outline.</br>
+4) Extrude this surface to set the stair width</br>
+5) Add railings </br>
+6) Transform model into component for ease of use. </br> 
+
+
 
 ## Log
+
+### 25/02/2025
+Repo renamed to stairs.rb, will focus on only one script.</br>
+Code now produces basic stair outline, rest to follow.</br>
+To add: </br>
+Stair surface, then extrude</br>
+UI limits (i.e., alerting wrong inputs on UI)</br>
+Railing</br>
+Save as component</br>
+
 
 ### 20/02/2025
 first upload - stairs.rb in progress
--- stairs.rb
+++ stairs.rb
@@ -1,42 +1,39 @@
-#Generate stairs
-#TODO everything!
-
-require langhandler.rb
 model = Sketchup.active_model
 ent = model.entities
-
+Sketchup.active_model.options
 
 #Create input box
-prompts = ["stair_ht", "min", "max", "step_length"]
-#defaults = ["Dialogue Title", "World Peace"]
-results = inputbox prompts
+# prompts = ["stair height", "minimum riser height", "maximum riser height", "step_length"]
+# results = inputbox prompts
 
-stair_ht = Float(results[0])
-min = Float(results[1])
-max = Float(results[2])
-step_length = Float(results[3])
+# stair_ht = Float(results[0])
+# min = Float(results[1])
+# max = Float(results[2])
+# step_length = Float(results[3])
 
+
+#Restriction clauses
+# if min < 150
+#   puts "minimum riser height is less than 130"
+# end
 
 #Get step heights
 def list(stair_ht, min, max, step_length)
-  riser_list = []
   avg_riser_ht = (min + max) / 2
-  step_number = stair_ht / avg_riser_ht
-  rem1 = stair_ht % avg_riser_ht
+  step_number = (stair_ht / avg_riser_ht).floor
+  rem1 = stair_ht % (avg_riser_ht)
   step_add = rem1 / (step_number - 1)
   rem2 = rem1 % (step_number - 1)
-  riser_init = avg_riser_ht + rem2
+  riser_init = stair_ht - ((step_number - 1) * (avg_riser_ht + step_add))
   riser_rest = avg_riser_ht + step_add
   tread = step_length
-  
+   
   def riser_tread(riser_init, riser_rest, step_number, tread)
     steps_list = [ [0, 0, 0], [0, 0, riser_init], [0, tread, riser_init], [0, tread, riser_init + riser_rest]]
-    points_list = []
     riser_start = riser_init + riser_rest
-    tread_start = tread
-    
+    tread_start = tread    
         steps = 0
-        while steps < step_number
+        while steps < ((step_number * 2) -3)
           steps += 1
   
             if steps % 2 == 0
@@ -49,19 +46,24 @@ def list(stair_ht, min, max, step_length)
   
             end          
         end
-  
+    pt_anchor1 =[0, tread, 0]
+    Geom::Point3d.new (steps_list.last)
 
-        Sketchup.active_model.entities.add_line (steps_list)
+
+        
+    Sketchup.active_model.entities.add_line (steps_list)
+    # Sketchup.active_model.entities.add_line pt_anchor1, pt_anchor2
+        p steps_list
   end
   riser_tread(riser_init, riser_rest, step_number, tread)
 
 end
 
 
-list(stair_ht, min, max, step_length)
+# list(stair_ht.mm, min.mm, max.mm, step_length.mm)
 
-
-  
+# list(stair_ht, min, max, step_length)
+list(2463.mm, 170.mm, 180.mm, 240.mm)
 
 
 
